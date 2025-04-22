b5b8a35fabbf0e5ab61f4044ea6864ba1bcd220d
nikhil mahajan <nikhilmahajan056@gmail.com>
2025-04-22 15:09:29 +0530

    ui for signup and login

---
+++ src/app/api/sign-in/route.ts
@@ -0,0 +1,16 @@
+import { createClient } from '@/utils/supabase/server';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(request: NextRequest) {
+    const data : {email: string; password: string} = await request.json();
+    
+    const supabase = await createClient();
+    const { error } = await supabase.auth.signInWithPassword(data);
+    if (error) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+    return NextResponse.json(
+        { message: "Successfully signed in" },
+        { status: 200}
+    )
+}
